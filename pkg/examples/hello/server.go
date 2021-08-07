5071ce4c4ff9e0d268b1ab519e6fd9816bbf4dbc
Travis Ellett <travisellett@gmail.com>
2021-08-07 15:37:36 -0600

    extract out the common httpserver stuff

--- pkg/examples/hello/server.go
+++ pkg/examples/hello/server.go
@@ -1,40 +1,13 @@
 package hello
 
 import (
-	"context"
-	"net/http"
-
 	"github.com/tellett/gocode/pkg/examples/hello/handlers/greeter"
-	"github.com/tellett/gocode/pkg/examples/hello/handlers/health"
-	"github.com/tellett/gocode/pkg/examples/hello/handlers/terminator"
-	"github.com/tellett/gocode/pkg/logging/loggerfx"
+	"github.com/tellett/gocode/pkg/net/httpserver"
 
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 var Module = fx.Options(
-	fx.Provide(http.NewServeMux),
-	loggerfx.Module,
-	health.Module,
-	terminator.Module,
+	httpserver.Module,
 	greeter.Module,
-	fx.Invoke(registerHooks),
 )
-
-func registerHooks(
-	lifecycle fx.Lifecycle, mux *http.ServeMux, logger *zap.SugaredLogger,
-) {
-	lifecycle.Append(
-		fx.Hook{
-			OnStart: func(context.Context) error {
-				logger.Info("Listening on localhost:8080")
-				go http.ListenAndServe(":8080", mux)
-				return nil
-			},
-			OnStop: func(context.Context) error {
-				return logger.Sync()
-			},
-		},
-	)
-}
